15ae4d3f1870a96f83bcfe0aea031fec15e1a304
atacankais <atacankais@gmail.com>
2024-04-11 17:39:15 +0200

    done for the day

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      <Button colorScheme="red" variant="outline">
+        Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
